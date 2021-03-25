170461b4405ce1fa2b2e3a3efdbb81d02e6df53b
nihalshanp <pnihalshan94@gmail.com>
2021-03-25 12:36:49 +0530

    removed provider from app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import store from "./redux/store";
-import countStore from "./redux/countStore";
-import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <CakeContainer />
-      </Provider>
-      <Provider store={countStore}>
-        <Counter />
       </Provider>
     </div>
   );
